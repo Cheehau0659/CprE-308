8701faa2e9a7e1266be08f77f0e30c78fa20dea2
Seth Braddock <sethbrad@iastate.edu>
2021-02-18 20:16:46 -0600

    lab2

--- lab2/main1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	fork();
-	fork();
-	usleep(1);
-	printf("Process %d's parent process ID is %d\n", getpid(), getppid());
-	return 0;
-}
--- lab2/main6.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main() {
-    execl("/bin/ls", "ls", (char *)NULL);
-    printf("What happened?\n");
-}
