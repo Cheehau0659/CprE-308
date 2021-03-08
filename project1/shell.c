7d009558e586be2ce536bf351281185bd991184c
Seth Braddock <sethbrad@iastate.edu>
2021-03-08 17:32:53 -0600

    commit before submission

--- project1/shell.c
+++ project1/shell.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <signal.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 
-void cmdForeground(char* args[]);
-void cmdBackground(char* args[]);
-void parseArgs(char* input, char* args[]);
+// Runs command with execvp
+void runCommand(char* args[], bool background);
+// Parses input line into string array
+bool parseArgs(char* input, char* args[]);
+// Gets the last char of a string
 char lastChar(char*);
+
+int backProcesses = 0;
 
 //TODO free memory leaks, kill processes, comment
 int main(int argc, char* argv[]) {
@@ -20,21 +24,35 @@ int main(int argc, char* argv[]) {
     } else if(argc == 1) {
         prompt = "308sh";
     } else {
-        printf("Enter '-p <prompt>' as arguments for a custom prompt");
+        printf("\nEnter '-p <prompt>' as arguments for a custom prompt");
         return 0;
     }
 
     // User input infinite loop
     char* input = NULL;
     size_t len = 0;
+
+    pid_t backPID;
+    int backStatus;
+
     while (1) {
+        // Check background processes
+        backPID = waitpid(-1, &backStatus, WNOHANG);
+
+        if(backProcesses) {
+            if(WIFEXITED(backStatus)) {
+                printf("\nChild %d exited with status %d", backPID, WEXITSTATUS(backStatus));   
+                backProcesses--; 
+            }
+        }
+        
         printf("\n%s> ", prompt);
 
         // Get user input
         getline(&input, &len, stdin);
         strtok(input, "\n");
 
-        // ---------- Builtin Commands ---------------------------------------	 	 
+        // Native commands	 
         if (strcmp(input, "exit") == 0) {
             return 0;
         } else if (strcmp(input, "pid") == 0) {
@@ -47,7 +65,7 @@ int main(int argc, char* argv[]) {
         } else if (strncmp(input, "cd", 2) == 0) {
             if(strlen(input) > 2) {
                 char* path = strtok(input, " ");
-                path = strtok(NULL, "\0");
+                path = strtok(NULL, "\0"); // Grab the path
 
                 if(chdir(path) == 0) {
                     printf("New working directory: %s", getcwd(NULL, 0));
@@ -59,41 +77,43 @@ int main(int argc, char* argv[]) {
                 printf("Home directory: %s", getcwd(NULL, 0));
             }
         } else {
-            // ---------- Non-Builtin Commands--------------------------
-            char* args[20];
-            parseArgs(input, args);
+            // External commands
+            char* args[20]; // Arbitrary number for max arguments
+            bool background = parseArgs(input, args);
 
-            if(lastChar(args[0]) == '&') {
-                cmdBackground(args);
+            if(background) {
+                runCommand(args, true);
             } else {
-                cmdForeground(args);
+                runCommand(args, false);
             }
         }
     }
     return 1;
 }
 // ----------------------------------- Helper Functions ----------------------------------
-void cmdForeground(char* args[]) {
-    pid_t pid = fork(); // don't work atm
-    if(pid == 0) {      
-        printf("Child PID: %d", pid);
+void runCommand(char* args[], bool background) {
+    pid_t pid = fork();
+    int status;
 
+    if(pid == 0) {     
+        usleep(50); // I don't know why the child always is faster than the parent, regardless of sleep
         if(execvp(args[0], args) == -1) {
-            printf("That is not a valid command");
+            printf("That is not a valid command\n");
+            exit(1);
         }
     } else {
-        int* stat_loc;
-        pid_t child = waitpid(pid, stat_loc, 0);
-        int status = *stat_loc;
-        printf("Child %d exited with status %d", child, status);
+        printf("\nChild PID: %d", pid);
+    }
+    
+    if(!background) {
+        while (wait(&status) != pid); // Busy wait for foreground task
+        printf("\nChild %d exited with status %d", pid, WEXITSTATUS(status));    
+    } else {
+        backProcesses++;
     }
 }
 
-void cmdBackground(char* args[]) {
-    printf("Background processes not implemented yet");
-}
-
-void parseArgs(char* input, char* args[]) {
+bool parseArgs(char* input, char* args[]) {
     int i;
     for(i = 0; i < 20; i++) {
         args[i] = strsep(&input, " ");
@@ -101,6 +121,14 @@ void parseArgs(char* input, char* args[]) {
         if(args[i] == NULL) {
             break;
         }
+    }
+    if(lastChar(args[0]) == '&') { // Check if background task
+        int len = strlen(args[0]);
+        args[0][len-1] = '\0';
+
+        return true;
+    } else {
+        return false;
     }
 }
 
